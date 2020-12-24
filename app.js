114772a0a51e9a84963bc63564062f3d85da86df
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-24 10:33:10 -0800

    Update app.js

--- app.js
+++ app.js
@@ -147,8 +147,6 @@ app.post("/delete", (req, res) => {
 app.get("/about", (req, res) => {
 	res.render("about");
 });
-let port = process.env.PORT;
-if (port == null || port == "") {
-	port = 3000;
-}
-app.listen(port);
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Serveer up local port: 3000");
+});
